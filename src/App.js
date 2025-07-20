a941f55a23b421342ff300369b617989b6ec6068
Ege Gurel <ege_gurel35@hotmail.com>
2025-07-20 04:50:31 +0300

    Third modification

--- src/App.js
+++ src/App.js
@@ -3,21 +3,17 @@ import Nav from './Nav';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+
+ 
 function App() {
+ 
   return (
     <div className="App">
       Home Page
-      <Header>
-
-    </Header>
-    <Nav>
-     </Nav>
-     <Main>
-
-     </Main>
-     <Footer>
-
-     </Footer>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
     </div>
   );
 }
